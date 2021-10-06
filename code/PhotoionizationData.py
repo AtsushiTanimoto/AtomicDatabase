3ae55fec046add8cab8cd654616371d913c6b8aa
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-10-06 15:28:55 +0900

    Updated Photoionization

--- code/PhotoionizationData.py
+++ code/PhotoionizationData.py
@@ -7,8 +7,8 @@ class PhotoionizationData:
         self.bound_level_twoj     = 0
         self.ionized_level_index  = -1
         self.ionized_level_twoj   = 0
-        self.l                    = 0
         self.ionization_potential = 0.0
+        self.l                    = 0
         self.sigma                = 0.0
         self.gamma                = 0.0
         self.tau                  = 0.0
@@ -18,4 +18,15 @@ class PhotoionizationData:
         with open("../database01/{0:s}/{0:s}{1:02d}a.rr".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="r") as fin:
             with open("../database02/{0:s}/{0:s}{1:02d}.pi".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="w") as fout:
                 for line in fin.readlines():
-                    if len(line)==6:
+                    data = line.split()
+                    if len(data)==3:
+                        if data[0]=="NEGRID":
+                            pass
+                    
+                    elif len(data)==6:
+                        self.bound_level_index    =   int(data[0])
+                        self.bound_level_twoj     =   int(data[1])
+                        self.ionized_level_index  =   int(data[2])
+                        self.ionized_level_twoj   =   int(data[3])
+                        self.ionization_potential = float(data[4])
+                        self.l                    = float(data[5])
