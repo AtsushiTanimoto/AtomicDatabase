7deefcdb38f8c716c35442e8602a0f15465d517e
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-10-06 10:56:10 +0900

    Updated LineEmissivity

--- code/LineEmissivity.py
+++ code/LineEmissivity.py
@@ -13,8 +13,8 @@ class LineEmissivity:
         maximum_energy = 1.0e+04 # eV
         threshold      = 0.0e+00
         transitions    = [1, 2, 3, 4, 5, 6, 7, 201, 202, 301, 302, 303, 401, 402, 403, 404, 501, 502, 503, 504, 505, 601, 602, 603, 604, 605, 606, 701, 702, 703, 704, 705, 706, 707]
-        input_dir      = "../database02/{0:s}/{0:s}{1:02d}_spec".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number)
-        output_dir     = "../database02/{0:s}/{0:s}{1:02d}_line".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number)
+        input_dir      = "../database01/{0:s}/{0:s}{1:02d}_spec".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number)
+        output_dir     = "../database02/{0:s}/{0:s}{1:02d}_ln"  .format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number)
         subprocess.call("rm -r {0:s}".format(output_dir), shell=True)
         subprocess.call("mkdir {0:s}".format(output_dir), shell=True)
 
