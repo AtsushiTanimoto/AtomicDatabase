5e74c530fa975880751e5965360c133f008830ae
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-10-06 15:48:29 +0900

    Updated Photoionization

--- code/PhotoionizationData.py
+++ code/PhotoionizationData.py
@@ -1,4 +1,6 @@
+import numpy
 import pfac.fac
+import scipy.optimize
 
 
 class PhotoionizationData:
@@ -7,26 +9,40 @@ class PhotoionizationData:
         self.bound_level_twoj     = 0
         self.ionized_level_index  = -1
         self.ionized_level_twoj   = 0
-        self.ionization_potential = 0.0
         self.l                    = 0
+        self.ionization_potential = 0.0      
         self.sigma                = 0.0
         self.gamma                = 0.0
         self.tau                  = 0.0
     
 
+    def func(self, x, sigma, gamma, tau):
+        return sigma*((x/self.ionization_potential)**gamma)*numpy.exp(-x/tau)
+
+
     def write(self, atomic_number, electron_number):
         with open("../database01/{0:s}/{0:s}{1:02d}a.rr".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="r") as fin:
             with open("../database02/{0:s}/{0:s}{1:02d}.pi".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="w") as fout:
                 for line in fin.readlines():
-                    data = line.split()
-                    if len(data)==3:
-                        if data[0]=="NEGRID":
-                            pass
+                    data   =  line.split()
+                    cross  = []
+                    energy = []
+
+                    if len(data)==4:
+                        energy += [      float(data[0])]
+                        cross  += [1e-20*float(data[2])]
+
+                        if len(energy)==6:
+                            par        = scipy.optimize.curve_fit(func, energy, cross)
+                            self.sigma = par[0]
+                            self.gamma = par[1]
+                            self.tau   = par[2]
+
                     
                     elif len(data)==6:
                         self.bound_level_index    =   int(data[0])
                         self.bound_level_twoj     =   int(data[1])
                         self.ionized_level_index  =   int(data[2])
                         self.ionized_level_twoj   =   int(data[3])
-                        self.ionization_potential = float(data[4])
                         self.l                    = float(data[5])
+                        self.ionization_potential = float(data[4])
