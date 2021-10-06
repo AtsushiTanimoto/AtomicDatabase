bd57817acad6e04855918d3b95d2c15f59d90fe6
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-10-06 13:08:15 +0900

    Updated LineProbability

--- code/LineProbability.py
+++ code/LineProbability.py
@@ -22,7 +22,7 @@ class LineProbability:
                         coefficient += [float(data[2])]
 
                 with open("../database01/{0:s}/{0:s}{1:02d}_line/{0:s}{1:02d}a_t{2:02d}d{3:02d}i02.ln".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number, i, j), mode="r") as fin:
-                    with open("../database02/{0:s}/{0:s}{1:02d}_ln/{0:s}{1:02d}a_t{2:02d}d{3:02d}i02.ln".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number, i, j), mode="w") as fout:
+                    with open("../database02/{0:s}/{0:s}{1:02d}_ln/{0:s}{1:02d}_t{2:02d}d{3:02d}i02.ln".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number, i, j), mode="w") as fout:
                         for line in fin.readlines():
                             data = line.split()
 
@@ -32,4 +32,4 @@ class LineProbability:
                             self.transition_quantum_numbers = int(data[3])
                             self.transition_energy          = float(data[4])
                             self.probability                = float(data[6])/(coefficient[i]*densities[j])
-                            fout.write("{0:2d}  {1:6d}  {2:6d}    {3:4d}       {4:12.6e}     {5:10.4e}".format(self.num_electrons, self.lower_level_index, self.upper_level_index, self.transition_quantum_numbers, self.transition_energy, self.probability))
+                            fout.write("{0:2d}  {1:6d}  {2:6d}    {3:4d}       {4:12.6e}     {5:10.4e}\n".format(self.num_electrons, self.lower_level_index, self.upper_level_index, self.transition_quantum_numbers, self.transition_energy, self.probability))
