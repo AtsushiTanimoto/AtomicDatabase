e30ca17b5fbc12e437a8badd3a131e0d62efe7c0
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-10-06 10:22:51 +0900

    Updated PhotoionizationData

---
+++ code/PhotoionizationData.py
@@ -0,0 +1,11 @@
+class PhotoionizationData:
+    def __init__(self):
+        self.bound_level_index    = -1
+        self.bound_level_twoj     = 0
+        self.ionized_level_index  = -1
+        self.ionized_level_twoj   = 0
+        self.l                    = 0
+        self.ionization_potential = 0.0
+        self.sigma                = 0.0
+        self.gamma                = 0.0
+        self.tau                  = 0.0
