7b611ba4005532b1c4daf59e58f6b05b93b49440
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-10-06 10:52:10 +0900

    Updated TemperatureDensityGrid

--- code/TemperatureDensityGrid.py
+++ code/TemperatureDensityGrid.py
@@ -8,10 +8,10 @@ class TemperatureDensityGrid:
 
     def write(self, atomic_number, electron_number, temperatures, densities):
         with open("../database02/{0:s}/{0:s}{1:02d}.grid".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="w") as fout:
-            fout.write("# {0:02d} {1:02d}".format(len(temperatures), len(densities)))
+            fout.write("# {0:02d} {1:02d}\n".format(len(temperatures), len(densities)))
             
             for i in range(len(temperatures)):
-                fout.write(" kT   {0:02d}     {1:11.5e}".format(i, temperatures[i]))
+                fout.write(" kT   {0:02d}     {1:11.5e}\n".format(i, temperatures[i]))
             
             for i in range(len(densities)):
-                fout.write(" ne   {0:02d}     {1:11.5e}".format(i, densities[i]))
+                fout.write(" ne   {0:02d}     {1:11.5e}\n".format(i, densities[i]))
