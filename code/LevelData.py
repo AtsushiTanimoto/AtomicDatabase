37bd9a4717e959f1848972cbe3c8d766a16d1354
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-10-06 10:29:58 +0900

    Updated LevelData

--- code/LevelData.py
+++ code/LevelData.py
@@ -14,9 +14,9 @@ class LevelData:
     
 
     def write(self, atomic_number, electron_number):
-        with open("../database01/{0:s}/{0:s}{1:02d}a.en".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="r") as infile:
-            with open("../database02/{0:s}/{0:s}{1:02d}.en".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="w") as outfile:
-                for line in infile.readlines():
+        with open("../database01/{0:s}/{0:s}{1:02d}a.en".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="r") as fin:
+            with open("../database02/{0:s}/{0:s}{1:02d}.en".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="w") as fout:
+                for line in fin.readlines():
                     data = line.split()
                     
                     if len(data)==3:
@@ -31,4 +31,4 @@ class LevelData:
                         self.nl                     =   int(data[4])
                         self.twoj                   =   int(data[5])
                         self.configuration          =   str(data[6])+" "+str(data[7])+" "+str(data[8])
-                        outfile.write("{0:2d}   {1:6d} {2:6d}    {3:14.8e}    {4:d}   {5:4d}   {6:3d}   \t{7:s}\n".format(self.num_electrons, self.level_index, self.level_index_of_ionized, self.level_energy, self.parity, self.nl, self.twoj, self.configuration))
+                        fout.write("{0:2d}   {1:6d} {2:6d}    {3:14.8e}    {4:d}   {5:4d}   {6:3d}   \t{7:s}\n".format(self.num_electrons, self.level_index, self.level_index_of_ionized, self.level_energy, self.parity, self.nl, self.twoj, self.configuration))
