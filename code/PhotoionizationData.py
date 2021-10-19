266cb56076eff399b3b01f6bb4414414843c6eb1
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-10-19 18:36:38 +0900

    Updated Photoionization

--- code/PhotoionizationData.py
+++ code/PhotoionizationData.py
@@ -35,7 +35,7 @@ class PhotoionizationData:
                     data   = line.split()
 
                     if len(data)==4:
-                        if data[1]!="Z":
+                        if data[1]!="Z" and self.bound_level_index in self.exist_level_index:
                             energy = numpy.append(energy, 1e+00*float(data[0])+self.ionization_potential)
                             cross  = numpy.append(cross , 1e-20*float(data[2]))
 
@@ -50,12 +50,11 @@ class PhotoionizationData:
                                 fout.write("{0:6d} {1:3d}   {2:6d} {3:3d}  {4:6d}      {5:11.5e}   {6:10.4e}  {7:10.4e}   {8:10.4e}\n".format(self.bound_level_index, self.bound_level_twoj, self.ionized_level_index, self.ionized_level_twoj, self.l, self.ionization_potential, self.sigma, self.gamma, self.tau))
  
                     elif len(data)==6:
-                        if int(data[0]) in self.exist_level_index:
-                            energy                    = numpy.array([])
-                            cross                     = numpy.array([])
-                            self.bound_level_index    =   int(data[0])
-                            self.bound_level_twoj     =   int(data[1])
-                            self.ionized_level_index  =   int(data[2])
-                            self.ionized_level_twoj   =   int(data[3])
-                            self.l                    =   int(data[5])
-                            self.ionization_potential = float(data[4])
+                        energy                    = numpy.array([])
+                        cross                     = numpy.array([])
+                        self.bound_level_index    =   int(data[0])
+                        self.bound_level_twoj     =   int(data[1])
+                        self.ionized_level_index  =   int(data[2])
+                        self.ionized_level_twoj   =   int(data[3])
+                        self.l                    =   int(data[5])
+                        self.ionization_potential = float(data[4])
