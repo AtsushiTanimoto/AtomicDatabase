562d933b58d31baea7923bc606f15af9c495625d
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-10-19 18:20:39 +0900

    Updated Radiativedecay

--- code/RadiativedecayData.py
+++ code/RadiativedecayData.py
@@ -1,30 +1,38 @@
+import pandas
 import pfac.fac
 
 
 class RadiativedecayData:
     def __init__(self):
-        self.upper_level_index              = -1
-        self.upper_level_statistical_weight = 0
-        self.lower_level_index              = -1
-        self.lower_level_statistical_weight = 0
-        self.transition_energy              = 0.0
-        self.oscillator_strength            = 0.0
-        self.radiative_decay_rate           = 0.0
+        self.upper_level_index              = []
+        self.upper_level_statistical_weight = []
+        self.lower_level_index              = []
+        self.lower_level_statistical_weight = []
+        self.transition_energy              = []
+        self.oscillator_strength            = []
+        self.radiative_decay_rate           = []
     
 
     def write(self, atomic_number, electron_number):
         with open("../database01/{0:s}/{0:s}{1:02d}a.tr".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="r") as fin:
-            with open("../database02/{0:s}/{0:s}{1:02d}.rd".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="w") as fout:
-                for line in fin.readlines():
-                    data = line.split()
+            for line in fin.readlines():
+                data = line.split()
                     
-                    if len(data)==8:
-                        if 1e-03<=float(data[5])/(1+int(data[3])):
-                            self.upper_level_index              =   int(data[0])
-                            self.upper_level_statistical_weight = 1+int(data[1])
-                            self.lower_level_index              =   int(data[2])
-                            self.lower_level_statistical_weight = 1+int(data[3])
-                            self.transition_energy              = float(data[4])
-                            self.oscillator_strength            = float(data[5])/(1+int(data[3]))
-                            self.radiative_decay_rate           = float(data[6])
-                            fout.write("{0:6d} {1:4d}   {2:6d} {3:4d}     {4:12.6e}  {5:12.6e}  {6:12.6e}\n".format(self.upper_level_index, self.upper_level_statistical_weight, self.lower_level_index, self.lower_level_statistical_weight, self.transition_energy, self.oscillator_strength, self.radiative_decay_rate))
+                if len(data)==8:
+                    if 1e-03<=float(data[5])/(1+int(data[3])):
+                        self.upper_level_index              += [int(data[0])]
+                        self.upper_level_statistical_weight += [1+int(data[1])]
+                        self.lower_level_index              += [int(data[2])]
+                        self.lower_level_statistical_weight += [1+int(data[3])]
+                        self.transition_energy              += [float(data[4])]
+                        self.oscillator_strength            += [float(data[5])/(1+int(data[3]))]
+                        self.radiative_decay_rate           += [float(data[6])]
+        
+        df = pandas.DataFrame({"upper_level_index": self.upper_level_index, "upper_level_index_statistical_weight": self.upper_level_statistical_weight, "lower_level_index": self.lower_level_index, "lower_level_statistical_weight": self.lower_level_statistical_weight, "transition_energy": self.transition_energy, "oscillator_strength": self.oscillator_strength, "radiative_decay_rate": self.radiative_decay_rate})
+        df.sort_values("upper_level_index", inplace=True)
+        df.reset_index(drop=True, inplace=True)
+        print(df)
+
+        with open("../database02/{0:s}/{0:s}{1:02d}.rd".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="w") as fout:
+            for i in range(len(df.index)):
+                fout.write("{0:6d} {1:4d}   {2:6d} {3:4d}     {4:12.6e}  {5:12.6e}  {6:12.6e}\n".format(df.upper_level_index[i], df.upper_level_statistical_weight[i], df.lower_level_index[i], df.lower_level_statistical_weight[i], df.transition_energy[i], df.oscillator_strength[i], df.radiative_decay_rate[i]))
