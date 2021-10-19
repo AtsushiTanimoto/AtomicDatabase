aec4a397fa5c9a03d42ed36e5cb9d1870e26aae9
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-10-19 18:30:38 +0900

    Updated Photoionization

--- code/PhotoionizationData.py
+++ code/PhotoionizationData.py
@@ -5,6 +5,7 @@ import scipy.optimize
 
 class PhotoionizationData:
     def __init__(self):
+        self.exist_level_index    = set()
         self.bound_level_index    = -1
         self.bound_level_twoj     = 0
         self.ionized_level_index  = -1
@@ -20,7 +21,14 @@ class PhotoionizationData:
         return y - param[0]*((x/self.ionization_potential)**param[1])*numpy.exp(-x/param[2])
 
  
-    def write(self, atomic_number, electron_number):
+    def write(self, atomic_number, electron_number, temperatures, densities):
+        for i in range(len(temperatures)):
+            for j in range(len(densities)):
+                with open("../database02/{0:s}/{0:s}{1:02d}_pop/{0:s}{1:02d}_t{2:02d}d{3:02d}i02.pop".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number, i, j), mode="r") as fin:
+                    for line in fin.readlines():
+                        data = line.split()
+                        self.exist_level_index.update({int(data[0])})
+
         with open("../database01/{0:s}/{0:s}{1:02d}a.rr".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="r") as fin:
             with open("../database02/{0:s}/{0:s}{1:02d}.pi".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="w") as fout:
                 for line in fin.readlines():
@@ -40,7 +48,7 @@ class PhotoionizationData:
                             self.tau   = parameter[2]
                             fout.write("{0:6d} {1:3d}   {2:6d} {3:3d}  {4:6d}      {5:11.5e}   {6:10.4e}  {7:10.4e}   {8:10.4e}\n".format(self.bound_level_index, self.bound_level_twoj, self.ionized_level_index, self.ionized_level_twoj, self.l, self.ionization_potential, self.sigma, self.gamma, self.tau))
  
-                    elif len(data)==6:
+                    elif int(data[0]) in self.exist_level_index and len(data)==6:
                         energy                    = numpy.array([])
                         cross                     = numpy.array([])
                         self.bound_level_index    =   int(data[0])
