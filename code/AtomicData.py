aeae62be9031f5a4efc62d52cc0021a04cc88bb7
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-11-05 11:17:22 +0900

    Updated Atomicdata

--- code/AtomicData.py
+++ code/AtomicData.py
@@ -5,9 +5,8 @@ import subprocess
 
 if __name__=="__main__":
     for i in range(3,11):
-        for j in range(1,i):
-            atomic_symbol = pfac.fac.ATOMICSYMBOL[i]
-            directory     = "../database01/{0:s}/".format(atomic_symbol)
-            subprocess.call("rm -r {0:s}".format(directory), shell=True)
-            subprocess.call("mkdir {0:s}".format(directory), shell=True)
-            pfac.atom.atomic_data(nele=[j], asym=atomic_symbol, dir=directory)
+        atomic_symbol = pfac.fac.ATOMICSYMBOL[i]
+        directory     = "../database01/{0:s}/".format(atomic_symbol)
+        subprocess.call("rm -r {0:s}".format(directory), shell=True)
+        subprocess.call("mkdir {0:s}".format(directory), shell=True)
+        pfac.atom.atomic_data(nele=[range(1,i)], asym=atomic_symbol, dir=directory)
