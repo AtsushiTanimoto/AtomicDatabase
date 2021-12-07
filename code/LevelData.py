7a4c7281dee66b02faf81be87596202b4e8dff75
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-12-07 18:46:26 +0900

    Updated Leveldata

--- code/LevelData.py
+++ code/LevelData.py
@@ -1,4 +1,3 @@
-import pandas
 import pfac.fac
 
 
@@ -8,13 +7,15 @@ class LevelData:
         self.level_index            = -1
         self.level_index_of_ionized = -1
         self.level_energy           = 0.0
-        self.parity                 = -1
-        self.nl                     = -1
-        self.twoj                   = -1
+        self.parity                 = 0
+        self.nl                     = 0
+        self.twoj                   = 0
         self.configuration          = ""
     
 
-    def write(self, atomic_number, electron_number):
+    def generate(self, atomic_number, electron_number):
+        leveldata = []
+
         with open("../database01/{0:s}/{0:s}{1:02d}a.en".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="r") as fin:
             for line in fin.readlines():
                 data = line.split()
@@ -23,19 +24,21 @@ class LevelData:
                     if str(data[0])=="NELE":
                         electron_number = int(data[2])
                 elif len(data)==9:
-                    self.num_electrons          += [electron_number]
-                    self.level_index            += [int(data[0])]
-                    self.level_index_of_ionized += [-1]
-                    self.level_energy           += [float(data[2])]
-                    self.parity                 += [int(data[3])]
-                    self.nl                     += [int(data[4])]
-                    self.twoj                   += [int(data[5])]
-                    self.configuration          += [str(data[6])+" "+str(data[7])+" "+str(data[8])+" "]
+                    self.num_electrons          = electron_number
+                    self.level_index            = int(data[0])
+                    self.level_index_of_ionized = -1
+                    self.level_energy           = float(data[2])
+                    self.parity                 = int(data[3])
+                    self.nl                     = int(data[4])
+                    self.twoj                   = int(data[5])
+                    self.configuration          = str(data[6])+" "+str(data[7])+" "+str(data[8])+" "
+                    leveldata.append({"num_electrons":self.num_electrons, "level_index":self.level_index, "level_index_of_ionized":self.level_index_of_ionized, "level_energy":self.level_energy, "parity":self.parity, "nl":self.nl, "twoj":self.twoj, "configuration":self.configuration})
+        
+        return leveldata
 
-        df = pandas.DataFrame({"num_electrons":self.num_electrons, "level_index":self.level_index, "level_index_of_ionized":self.level_index_of_ionized, "level_energy":self.level_energy, "parity":self.parity, "nl":self.nl, "twoj":self.twoj, "configuration":self.configuration})
-        df.sort_values("level_index", inplace=True)
-        df.reset_index(drop=True, inplace=True)
-
+    
+    def write(self, atomic_number, electron_number):
+        leveldata = self.generate(atomic_number, electron_number)
         with open("../database02/{0:s}/{0:s}{1:02d}.en".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="w") as fout:
-            for i in range(len(df.index)):
-                fout.write("{0:2d}   {1:6d} {2:6d}    {3:14.8e}    {4:d}   {5:4d}   {6:3d}   \t{7:s}\n".format(df.num_electrons[i], df.level_index[i], df.level_index_of_ionized[i], df.level_energy[i], df.parity[i], df.nl[i], df.twoj[i], df.configuration[i]))
+            for i in range(len(leveldata)):
+                fout.write("{0:2d}   {1:6d} {2:6d}    {3:14.8e}    {4:d}   {5:4d}   {6:3d}   \t{7:s}\n".format(leveldata[i]["num_electrons"], leveldata[i]["level_index"], leveldata[i]["level_index_of_ionized"], leveldata[i]["level_energy"], leveldata[i]["parity"], leveldata[i]["nl"], leveldata[i]["twoj"], leveldata[i]["configuration"]))
