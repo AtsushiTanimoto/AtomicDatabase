14a4ae683394361e9022f241c28985a21cb1d1d5
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-12-07 16:18:11 +0900

    Updatef database

--- code/database.py
+++ code/database.py
@@ -23,7 +23,7 @@ if __name__=="__main__":
     logger.setLevel(logging.DEBUG)
     logger.addHandler(handler)
 
-    for i in range(3,4):
+    for i in range(26,27):
         subprocess.call("rm -r ../database02/{0:s}".format(pfac.fac.ATOMICSYMBOL[i]), shell=True)
         subprocess.call("mkdir ../database02/{0:s}".format(pfac.fac.ATOMICSYMBOL[i]), shell=True)
             
