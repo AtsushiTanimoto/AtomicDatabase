9653925954c4229aa04a1e0fc166c434bc5a9080
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-12-07 18:54:47 +0900

    Updated Autoionization

--- code/AutoionizationData.py
+++ code/AutoionizationData.py
@@ -4,45 +4,51 @@ import pfac.fac
 
 class AutoionizationData:
     def __init__(self):
-        self.maximum_level_index = 0
-        self.bound_level_index   = []
-        self.bound_level_twoj    = []
-        self.ionized_level_index = []
-        self.ionized_level_twoj  = []
-        self.transition_energy   = []
-        self.autoionization_rate = []
+        self.bound_level_index   = -1
+        self.bound_level_twoj    = 0
+        self.ionized_level_index = -1
+        self.ionized_level_twoj  = 0
+        self.transition_energy   = 0.0
+        self.autoionization_rate = 0.0
     
 
-    def write(self, atomic_number, electron_number):
+    def generate(self, atomic_number, electron_number):
+        autoionization_data = []
+        maximum_level_index = 0
+
         if electron_number==1:
             pass
         else:
             with open("../database02/{0:s}/{0:s}{1:02d}.px".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="r") as fin:
                 for line in fin.readlines():
-                    data               = line.split()
-                    self.maximum_level_index = max(self.maximum_level_index, int(data[0]))
+                    data                = line.split()
+                    maximum_level_index = max(maximum_level_index, int(data[0]))
 
             with open("../database01/{0:s}/{0:s}{1:02d}a.ai".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="r") as fin:
                 for line in fin.readlines():
-                        data = line.split()
+                    data = line.split()
 
-                        if len(data)==7:
-                            if int(data[0])<=self.maximum_level_index:
-                                self.bound_level_index   += [int(data[0])]
-                                self.bound_level_twoj    += [int(data[1])]
-                                self.ionized_level_index += [int(data[2])]
-                                self.ionized_level_twoj  += [int(data[3])]
-                                self.transition_energy   += [float(data[4])]
-                                self.autoionization_rate += [float(data[5])]
-                
-            df = pandas.DataFrame({"bound_level_index":self.bound_level_index, "bound_level_twoj":self.bound_level_twoj, "ionized_level_index":self.ionized_level_index, "ionized_level_twoj":self.ionized_level_twoj, "transition_energy":self.transition_energy, "autoionization_rate":self.autoionization_rate})
-            df.sort_values("bound_level_index", inplace=True)
-            df.reset_index(drop=True, inplace=True)
-            
+                    if len(data)==7:
+                        if int(data[0])<=self.maximum_level_index:
+                            self.bound_level_index   = int(data[0])
+                            self.bound_level_twoj    = int(data[1])
+                            self.ionized_level_index = int(data[2])
+                            self.ionized_level_twoj  = int(data[3])
+                            self.transition_energy   = float(data[4])
+                            self.autoionization_rate = float(data[5])
+
+                autoionization_data.append({"bound_level_index":self.bound_level_index, "bound_level_twoj":self.bound_level_twoj, "ionized_level_index":self.ionized_level_index, "ionized_level_twoj":self.ionized_level_twoj, "transition_energy":self.transition_energy, "autoionization_rate":self.autoionization_rate})
+        
+        return autoionization_data
+        
+
+    def write(self, atomic_number, electron_number):
+        autoionization_data = self.generate(atomic_number, electron_number)
+
         if electron_number==1:
             with open("../database02/{0:s}/{0:s}{1:02d}.ai".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="w") as fout:
                 pass
         else:
             with open("../database02/{0:s}/{0:s}{1:02d}.ai".format(pfac.fac.ATOMICSYMBOL[atomic_number], electron_number), mode="w") as fout:
-                for i in range(len(df.index)):
-                    fout.write("{0:6d} {1:3d}   {2:6d} {3:3d}       {4:10.4e}    {5:10.4e}\n".format(self.bound_level_index[i], self.bound_level_twoj[i], self.ionized_level_index[i], self.ionized_level_twoj[i], self.transition_energy[i], self.autoionization_rate[i]))
+                for i in range(len(autoionization_data)):
+                    fout.write("{0:6d} {1:3d}   {2:6d} {3:3d}       {4:10.4e}    {5:10.4e}\n".format(autoionization_data[i]["bound_level_index"], autoionization_data[i]["bound_level_twoj"], autoionization_data[i]["ionized_level_index"], autoionization_data[i]["ionized_level_twoj"], autoionization_data[i]["transition_energy"], autoionization_data[i]["autoionization_rate"]))
