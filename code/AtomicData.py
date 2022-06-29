d217f2574637a14da99e09a11bec4100484709e6
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-06-29 14:40:39 +0900

    Changes to be committed: 	modified:   AtomicData.py
     Changes not staged for commit:
    	modified:   Spectrum.py

--- code/AtomicData.py
+++ code/AtomicData.py
@@ -1,12 +1,15 @@
+from mpi4py import MPI
 import pfac.atom
 import pfac.fac
 import subprocess
 
 
 if __name__=="__main__":
-    for i in range(30,31):
-        atomic_symbol = pfac.fac.ATOMICSYMBOL[i]
-        directory     = "../database01/{0:s}/".format(atomic_symbol)
-        subprocess.run("rm -r {0:s}".format(directory), shell=True)
-        subprocess.run("mkdir {0:s}".format(directory), shell=True)
-        pfac.atom.atomic_data(nele=range(1,min(11,i)), asym=atomic_symbol, dir=directory)
+    comm          = MPI.COMM_WORLD
+    rank          = comm.Get_rank()
+    size          = comm.Get_size()
+    atomic_symbol = pfac.fac.ATOMICSYMBOL[1+rank]
+    directory     = "/Users/tanimoto/github/AtomicDatabase/database01/{0:s}/".format(atomic_symbol)
+    subprocess.run("rm -r {0:s}".format(directory), shell=True)
+    subprocess.run("mkdir {0:s}".format(directory), shell=True)
+    pfac.atom.atomic_data(nele=range(1,min(11,1+rank)), asym=atomic_symbol, dir=directory)
